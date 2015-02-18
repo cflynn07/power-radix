ead7a4413207583f6e6856265f2b2712e89e5c6d
Casey Flynn <cflynn.us@gmail.com>
2015-02-17 19:27:06 -0800

    base class

---
+++ index.js
@@ -0,0 +1,39 @@
+/**
+ * Main module
+ * @module power-radix
+ */
+'use strict';
+
+/* Constructor */
+module.exports = PowerRadix;
+
+/**
+ * Class encapsulating radix conversion operations
+ * @class
+ * @param {number|string|array} number
+ * @param {number} radix
+ * @return this
+ */
+function PowerRadix (number, radix) {
+  this.number = number;
+  this.radix = radix;
+  return this;
+}
+
+/**
+ * Output array representation of number converted to outputRadix
+ * @param {number} outputRadix
+ * @return this
+ */
+PowerRadix.prototype.toArray = function (outputRadix) {
+  return [''];
+};
+
+/**
+ * Output string representation of number converted to outputRadix
+ * @param {number} outputRadix
+ * @return this
+ */
+PowerRadix.prototype.toString = function (outputRadix) {
+  return '';
+};
