8d1beb78ac1534044fdce01e754b23a1ae011c74
Casey Flynn <cflynn.us@gmail.com>
2015-03-17 11:45:49 -0700

    fixtures, sample character sets for radix encodings

---
+++ test/fixtures/encodings.js
@@ -0,0 +1,52 @@
+/**
+ * Sample encoding character sets for tests
+ * @module test/fixtures/encodings
+ */
+
+module.exports.base10 = [
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9
+];
+
+module.exports.base16 = [
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F'
+];
+
+module.exports.base58 = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M',
+  'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm',
+  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+  1, 2, 3, 4, 5, 6, 7, 8, 9
+];
+
+module.exports.base64 = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+  'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '+', '/'
+];
