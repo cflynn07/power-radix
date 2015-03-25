16ef029c9824bb194ebd07dd2b5ee4ad6a11abe1
Casey Flynn <cflynn.us@gmail.com>
2015-03-24 23:31:17 -0700

    WIP bitcoin address & base58check encoding example

--- examples/bitcoinAddress.js
+++ examples/bitcoinAddress.js
@@ -8,39 +8,40 @@
 'use strict';
 
 var base16Encoding = [
-  '0',
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f'
+  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+  'a', 'b', 'c', 'd', 'e', 'f'
 ];
 
+//var PowerRadix = require('../lib/index');
+var Table = require('cli-table');
 var crypto = require('crypto');
 var ecurve = require('ecurve');
-var PowerRadix = require('../lib/index');
+var BigInt = require('bigi');
+
+var table = new Table();
+
+/**
+ * Create private key
+ */
+var privateKey = crypto.createHash('sha256')
+  .update(Math.random()+'')
+  .digest('hex');
 
 var ecparams = ecurve.getCurveByName('secp256k1');
-var shasum = crypto.createHash('sha256');
+// using eliptic-curve arithmetic, multiply private key against
+// the gernator-point on the eliptic curve to get the point on the
+// curve that will correlate to a public-key
+var ECPoint = ecparams.G.multiply(BigInt.fromHex(privateKey));
+var x = ECPoint.affineX.toBuffer(32);
+var y = ECPoint.affineY.toBuffer(32);
 
-// Generate a random, new private key
-shasum.update(Math.random()+'');
+// previx point on curve with version byte (4)
+var prefixedCurvePoint = Buffer.concat(new Buffer([0x04], x, y));
+// NOTE: will create example explaining compressed public key
 
-// digest accepts 'hex', 'binary', or 'base64'
-var privateKey = shasum.digest('hex');
 
-// just for fun lets convert privateKey from base16 to base 2 and log
-console.log('privateKey', privateKey);
-var powerRadix = new PowerRadix(privateKey, base16Encoding);
-var privateKeyBase2 = powerRadix.toArray([0, 1]);
-console.log('privateKeyBase2', privateKeyBase2);
+table.push([
+  'private key (hex)', privateKey
+]);
+
+console.log(table.toString());
